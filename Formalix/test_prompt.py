6087928a8feb30d14f29ee32838410dc495e7bbd
dodekagon <philipp.sedlmeier@tum.de>
2021-04-17 21:37:32 +0200

    real method

--- Formalix/test_prompt.py
+++ Formalix/test_prompt.py
@@ -7,11 +7,13 @@ openai.api_key = os.getenv("OPEN_API_KEY")
 openai.Engine.list()
 
 trials = 3
-max_tokens=64
+max_tokens = 64
+
 
 def logprob_to_prob(logits):
     odds = np.exp(logits)
     return odds / (1 + odds)
+
 
 def try_instruct_request():
     prompt = "Write a paragraph in academic style that includes the terms "
@@ -26,19 +28,19 @@ def try_instruct_request():
         frequency_penalty=0.8,
         logprobs=4,
         n=trials)
-    #logprobs = [result['choices'][i]['logprobs'] for i in range(trials)]
-    #print(len(logprobs[0]))
-    #print(logprobs[0])
-    #print(result)
+    # logprobs = [result['choices'][i]['logprobs'] for i in range(trials)]
+    # print(len(logprobs[0]))
+    # print(logprobs[0])
+    # print(result)
     return result
 
 
-
-desired_result="""
+desired_result = """
 The NASA Prediction Of Worldwide Energy Resources project (POWER) provides plenty of solar and meteorological data sets that can be accessed through a restful API.
 The currently available version is its Release 8.
 Solar parameters are available from July 1983 onwards, although from different data sources.
 """
+
 
 def tryRequest():
     prompt = """
@@ -80,26 +82,34 @@ def tryRequest():
         top_p=1,
         stop="###",
         frequency_penalty=0.8,
-        #logprobs=2,
+        # logprobs=2,
         n=3)
     print(result)
     print(result['choices'])
 
-#tryRequest()
+
+# Calculates how confident GPT-3 is about each response,
+# i.e. the average probability of the tokens in each response.
+# high value = high probability = high confidence
+def compute_confidence_per_result(response, trials=trials, max_tokens=max_tokens):
+    token_logprobs = []
+    for i in range(trials):
+        logprobs_i = np.asarray(result['choices'][i]['logprobs']['token_logprobs'])
+        # some results stop before consuming all tokens.
+        # fill that up with zeros, so we don't confuse numpy
+        logprobs_i.resize(max_tokens)
+        token_logprobs.append(logprobs_i)
+    logprobs = np.asarray(token_logprobs).reshape(trials, -1)
+    # Shouldn't use np.mean(1) here, because that'd include the padding zeros from above
+    log_confidence = np.true_divide(logprobs.sum(axis=1), (logprobs != 0).sum(axis=1))
+    return logprob_to_prob(log_confidence)
+
+
+# tryRequest()
 result = try_instruct_request()
-token_logprobs = []
-for i in range(trials):
-    logprobs_i = np.asarray(result['choices'][i]['logprobs']['token_logprobs'])
-    # some results stop before consuming all tokens.
-    # fill that up with zeros, so we don't confuse numpy
-    logprobs_i.resize(max_tokens)
-    token_logprobs.append(logprobs_i)
-logprobs = np.asarray(token_logprobs).reshape(trials, -1)
-# Shouldn't use np.mean(1) here, because that'd include the padding zeros from above
-log_confidence = np.true_divide(logprobs.sum(axis=1), (logprobs != 0).sum(axis=1))
-confidence_per_result = logprob_to_prob(log_confidence)
+
+confidence_per_result = compute_confidence_per_result(result)
 
 for i in range(trials):
     print(result['choices'][i]['text'])
     print("\nGPT-3 returned the result with confidence {}".format((confidence_per_result[i])))
-
